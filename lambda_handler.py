b2aae34ad1469934597f6e4e031e9d0cb76931f7
q-foley <quinnfoley@gmail.com>
2018-07-06 14:14:15 -0500

    got it working

--- lambda_handler.py
+++ lambda_handler.py
@@ -1,6 +1,7 @@
 import json
 import boto3
 from warrant import Cognito
+from six.moves.urllib.parse import parse_qs
 
 IDENTITY_POOL_ID = 'us-east-2:e2a5786c-5dc2-4025-84c8-e629568a0f32'
 USER_POOL_ID = 'us-east-2_a7vE0OvG5'
@@ -10,12 +11,21 @@ DEFAULT_PASSWORD = 'Testing123!'
 def lambda_handler(event, context):
         print ("Got event \n" + json.dumps(event,indent=2))
 
+        p_body = parse_qs(event['body'])
+
+        #print str(p_body['user'][0])
+        #print str(p_body['tenant_id'][0])
+        #print str(p_body['user_id'][0])
+        #print p_body['email_addr'][0]
+
+        print event
+
         cognito = boto3.client('cognito-idp',region_name='us-east-2')
-        if event['user'] != None:
-            user = event['user']
-            email_add = event['email']
-            t_id = event['tenant']
-            u_id = event['user_id']
+        if p_body['user'] != None:
+            user = p_body['user'][0]
+            email_add = p_body['email_addr'][0]
+            t_id = p_body['tenant_id'][0]
+            u_id = p_body['user_id'][0]
 
             u = Cognito(USER_POOL_ID, CLIENT_ID)
             u.add_base_attributes(email = email_add)
@@ -28,4 +38,7 @@ def lambda_handler(event, context):
             u.admin_authenticate(password=DEFAULT_PASSWORD)
             print "getting refresh token"
 
-            return u.refresh_token
+            return {
+                'statusCode':200,
+                'body': json.dumps({'refresh_token' : u.refresh_token})
+            }
