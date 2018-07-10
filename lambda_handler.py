b8f9e002e37aed72afdc809d20aef9743ffbe8b0
q-foley <quinnfoley@gmail.com>
2018-07-10 15:19:38 -0500

    added random password

--- lambda_handler.py
+++ lambda_handler.py
@@ -1,12 +1,24 @@
+import os
 import json
 import boto3
+import random
+import re
+import string
 from warrant import Cognito
 from six.moves.urllib.parse import parse_qs
 
-IDENTITY_POOL_ID = 'us-east-2:e2a5786c-5dc2-4025-84c8-e629568a0f32'
-USER_POOL_ID = 'us-east-2_a7vE0OvG5'
-CLIENT_ID = '6e1qos6n7r927dsob5mm23opdp'
-DEFAULT_PASSWORD = 'Testing123!'
+IDENTITY_POOL_ID = os.environ['IDENTITY_POOL_ID']
+USER_POOL_ID = os.environ['USER_POOL_ID']
+CLIENT_ID = os.environ['CLIENT_ID']
+
+def gen_random_string(n):
+    # Use underlying /dev/urandom as a source of randomness
+    s = os.urandom(n).encode('base64')
+    # Filter-out some unwanted characters
+    s = re.sub('[\n\r=]+', '', s)
+    # Truncate to requested length (yes, some
+    # cheracters are wasted ..)
+    return s[:n]
 
 def lambda_handler(event, context):
         print ("Got event \n" + json.dumps(event,indent=2))
@@ -27,19 +39,30 @@ def lambda_handler(event, context):
             t_id = p_body['tenant_id'][0]
             u_id = p_body['user_id'][0]
 
+            passwd = gen_random_string(8)
+            passwd = passwd + "!!" + str(random.randint(0,100))
+
+            print passwd
+
             u = Cognito(USER_POOL_ID, CLIENT_ID)
             u.add_base_attributes(email = email_add)
             u.add_custom_attributes(tenantId=t_id, userId=u_id, lib_view = '*')
-            u.register(user,DEFAULT_PASSWORD)
+            u.register(user,passwd)
 
             response = cognito.admin_confirm_sign_up(UserPoolId = USER_POOL_ID,Username = user)
             u = Cognito(USER_POOL_ID,CLIENT_ID,username = user)
 
-            u.admin_authenticate(password=DEFAULT_PASSWORD)
+            u.admin_authenticate(password=passwd)
             print "getting refresh token"
 
             return {
                 'statusCode': 200,
                 'headers': {'Content-Type': 'application/json'},
                 'body': json.dumps({'refresh_token' : u.refresh_token})
+            }
+        else:
+            return {
+                'statusCode': 403,
+                'headers': {'Content-Type': 'application/json'},
+                'body': json.dumps({'message': 'invalid user input'})
             }
